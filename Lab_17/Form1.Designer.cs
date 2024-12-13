23d544c2766e0dfbbe2cc3cc5f1eebcc36775488
nae-stormrage <stas.markus.2004@mail.ru>
2024-12-13 21:50:29 +0300

    Добавьте файлы проекта.

---
+++ Lab_17/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Lab_17
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnChooseColor = new System.Windows.Forms.Button();
+            this.btnOnAnim = new System.Windows.Forms.Button();
+            this.btnOffAnim = new System.Windows.Forms.Button();
+            this.MainTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnChooseColor
+            // 
+            this.btnChooseColor.Location = new System.Drawing.Point(12, 325);
+            this.btnChooseColor.Name = "btnChooseColor";
+            this.btnChooseColor.Size = new System.Drawing.Size(95, 34);
+            this.btnChooseColor.TabIndex = 0;
+            this.btnChooseColor.Text = "Сменить цвет";
+            this.btnChooseColor.UseVisualStyleBackColor = true;
+            this.btnChooseColor.Click += new System.EventHandler(this.btnChooseColor_Click);
+            // 
+            // btnOnAnim
+            // 
+            this.btnOnAnim.Location = new System.Drawing.Point(113, 325);
+            this.btnOnAnim.Name = "btnOnAnim";
+            this.btnOnAnim.Size = new System.Drawing.Size(95, 34);
+            this.btnOnAnim.TabIndex = 1;
+            this.btnOnAnim.Text = "Включение анимации";
+            this.btnOnAnim.UseVisualStyleBackColor = true;
+            this.btnOnAnim.Click += new System.EventHandler(this.btnOnAnim_Click);
+            // 
+            // btnOffAnim
+            // 
+            this.btnOffAnim.Location = new System.Drawing.Point(214, 325);
+            this.btnOffAnim.Name = "btnOffAnim";
+            this.btnOffAnim.Size = new System.Drawing.Size(95, 34);
+            this.btnOffAnim.TabIndex = 2;
+            this.btnOffAnim.Text = "Выключение анимации";
+            this.btnOffAnim.UseVisualStyleBackColor = true;
+            this.btnOffAnim.Click += new System.EventHandler(this.btnOffAnim_Click);
+            // 
+            // MainTimer
+            // 
+            this.MainTimer.Interval = 500;
+            this.MainTimer.Tick += new System.EventHandler(this.MainTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.btnOffAnim);
+            this.Controls.Add(this.btnOnAnim);
+            this.Controls.Add(this.btnChooseColor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChooseColor;
+        private System.Windows.Forms.Button btnOnAnim;
+        private System.Windows.Forms.Button btnOffAnim;
+        private System.Windows.Forms.Timer MainTimer;
+    }
+}
+
---
+++ Lab_17/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Lab_17
+{
+    public partial class Form1 : Form
+    {
+        Region region1;
+        Region region2;
+        SolidBrush solid_gray_pen = new SolidBrush(Color.Gray);
+        Point point_x = new Point(275, 160);
+        Point point_y = new Point(130, 25);
+        bool draggingX = false;
+        bool draggingY = false;
+        Point offset;
+        double T = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen pen = new Pen(Color.Black,3);
+            SolidBrush solid_pen = new SolidBrush(Color.Black);
+
+            // Закрашивание области 1
+            GraphicsPath leftBottomCircle1 = new GraphicsPath();
+            GraphicsPath leftBottomCircle2 = new GraphicsPath();
+            leftBottomCircle1.AddEllipse(150, 150, 100, 100);
+            leftBottomCircle2.AddEllipse(100, 100, 100, 100);
+            Region regionCircle_1 = new Region(leftBottomCircle1);
+            Region regionCircle_2 = new Region(leftBottomCircle2);
+            regionCircle_1.Intersect(regionCircle_2);
+            g.FillRegion(solid_gray_pen, regionCircle_1);
+            region1 = regionCircle_1;
+
+            // Закрашивание области 2
+            Rectangle rect = new Rectangle(50, 50, 200, 200);
+            GraphicsPath pie = new GraphicsPath();
+            GraphicsPath circle = new GraphicsPath();
+            GraphicsPath circle2 = new GraphicsPath();
+            pie.AddPie(rect, 90, 45);
+            circle.AddEllipse(100, 100, 100, 100);
+            circle.AddEllipse(50, 50, 200, 200);
+            circle2.AddEllipse(50, 150, 100, 100);
+            Region regionPie = new Region(pie);
+            Region regionCircle = new Region(circle);
+            Region regionCircle2 = new Region(circle2);
+            regionPie.Intersect(regionCircle);
+            regionPie.Intersect(regionCircle2);
+            g.FillRegion(solid_gray_pen, regionPie);
+            region2 = regionPie;
+
+
+            // Рисование линий X, Y и Диагональ
+            pen.DashStyle = DashStyle.Solid;
+            g.DrawLine(pen, 25, 150, 275, 150);
+            g.DrawLine(pen, 150, 25, 150, 275);
+            g.DrawLine(pen, 50, 250, 250, 50);
+
+            // Рисование стрелочек на X и Y
+            Point point1 = new Point(275, 145);
+            Point point2 = new Point(275, 155);
+            Point point3 = new Point(285, 150);
+            Point[] massivPoints = { point1, point2, point3 };
+            g.FillPolygon(solid_pen, massivPoints);
+            Point point4 = new Point(145, 25);
+            Point point5 = new Point(155, 25);
+            Point point6 = new Point(150, 15);
+            Point[] massivPoints2 = { point4, point5, point6 };
+            g.FillPolygon(solid_pen, massivPoints2);
+
+            // Рисование кругов говна
+            pen.DashStyle = DashStyle.Solid;
+            g.DrawEllipse(pen, 50, 50, 200, 200);
+            g.DrawEllipse(pen, 100, 100, 100, 100);
+            g.DrawEllipse(pen, 50, 150, 100, 100);
+            g.DrawEllipse(pen, 150, 150, 100, 100);
+
+            // Рисование пунктирных линий
+            pen.DashStyle = DashStyle.Dash;
+            g.DrawLine(pen, 150, 50, 250, 50);
+            g.DrawLine(pen, 250, 50, 250, 150);
+
+            // Рисование текста X и Y
+            String string_x = "X";
+            String string_y = "Y";
+            Font font = new Font("Times New Roman", 14);
+            g.DrawString(string_x, font, solid_pen, point_x);
+            g.DrawString(string_y, font, solid_pen, point_y);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                Text = region1.IsVisible(e.Location).ToString() + " " + region2.IsVisible(e.Location).ToString();
+            }
+            catch { }
+
+            if (region1.IsVisible(e.Location) == true)
+            {
+                solid_gray_pen = new SolidBrush(Color.Red);
+                Invalidate(region1);
+            }
+            else if (region2.IsVisible(e.Location) == true)
+            {
+                solid_gray_pen = new SolidBrush(Color.Green);
+                Invalidate(region2);
+            }
+
+            if (draggingX == true)
+            {
+                point_x = new Point(e.X - offset.X, e.Y - offset.Y);
+                Invalidate();
+            }
+            else if (draggingY == true)
+            {
+                point_y = new Point(e.X - offset.X, e.Y - offset.Y);
+                Invalidate();
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Size textSize = TextRenderer.MeasureText("X", new Font("Times New Roman", 14));
+            Rectangle rectX = new Rectangle(point_x, textSize);
+            Rectangle rectY = new Rectangle(point_y, textSize);
+
+            if (rectX.Contains(e.Location))
+            {
+                draggingX = true;
+                offset = new Point(e.X - point_x.X, e.Y - point_x.Y);
+            }
+            else if (rectY.Contains(e.Location))
+            {
+                draggingY = true;
+                offset = new Point(e.X - point_y.X, e.Y - point_y.Y);
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            draggingX = false;
+            draggingY = false;
+        }
+
+        private void btnChooseColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog dlg = new ColorDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                solid_gray_pen.Color = dlg.Color;
+                Invalidate();
+            }
+        }
+
+        private void btnOnAnim_Click(object sender, EventArgs e)
+        {
+            MainTimer.Start();
+        }
+
+        private void btnOffAnim_Click(object sender, EventArgs e)
+        {
+            point_x = new Point(275, 160);
+            Invalidate();
+            MainTimer.Stop();
+        }
+
+        private void MainTimer_Tick(object sender, EventArgs e)
+        {
+            T += 0.5;
+            point_x.X = (int)(275 + 20 * Math.Sin(T));
+            point_x.Y = (int)(160 + 20 * Math.Cos(T));
+            point_y.X = (int)(130 + 20 * Math.Sin(T));
+            point_y.Y = (int)(25 + 20 * Math.Cos(T));
+            Invalidate();
+        }
+    }
+}
